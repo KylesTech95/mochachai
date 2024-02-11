8cec9034f91356de33e92c9c14039c2dd925163e
kylestech95 <kylestech95@outlook.com>
2024-02-11 07:19:21 -0500

    fix: function

--- tests/copy_unit-test.js
+++ tests/copy_unit-test.js
@@ -4,7 +4,7 @@ const assert = chai.assert;
 suite('Unit Tests',()=>{
     suite('Basic Assertions',()=>{
         // #1
-        test('#isNull, #isNotNull',(){
+        test('#isNull, #isNotNull',()=>{
             assert.isNull(null, 'This is an optional error description: null is null')
             assert.isNotNull(1,'1 is not null');
         });
